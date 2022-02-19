7f3bcd63eece456238021b6e433b4a62accb7bd3
kiran.ks <kiran.ks@inbl01lap7243.brd.g-c-i.com>
2022-02-19 12:53:13 +0530

    Assessments

---
+++ Assessment_Capstone/src/test/java/TestCases/ManageApointPageTest.java
@@ -0,0 +1,39 @@
+package TestCases;
+
+import java.io.IOException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import Demo_MRS_Pages.HomePage;
+import Demo_MRS_Pages.LoginPage;
+import Demo_MRS_Pages.ManageAppointPage;
+import Package_Base.BaseClass;
+
+public class ManageApointPageTest extends BaseClass {
+	public LoginPage lp;
+	public HomePage homePage;
+	public ManageAppointPage ma;
+	
+	public ManageApointPageTest() throws IOException {
+		super();
+	}
+	
+	@BeforeMethod
+	public void SetUp() throws IOException {
+		setup();
+		lp = new LoginPage();
+	}
+	
+	@Test
+	public void verify_x_clickButton() throws InterruptedException, IOException {
+		homePage = lp.login(prop.getProperty("username"),prop.getProperty("password"));
+		ma=new ManageAppointPage();
+		ma.clickOnNo();
+		
+	}
+	
+	/*@AfterTest
+	public void TearDown() throws IOException {
+		ma.teardown();
+	}*/
+}
